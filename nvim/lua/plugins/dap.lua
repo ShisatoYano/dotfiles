4a8deb31991d4c148f1cfdd37d0588a954013bbe
Shisato Yano <yano@8kt.jp>
2026-07-06 21:21:03 +0900

    feat: 🎸 add setting for C++/Python code debug
    Added init.lua for defining leader key and changed dap.lua for adding
    debug settings

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,6 +1,63 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- C++用: codelldb
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+          args = { "--port", "${port}" },
+        },
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input(
+              "実行ファイルのパス: ",
+              vim.fn.getcwd() .. "/build/",
+              "file"
+            )
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+
+      -- Python用: debugpy
+      dap.adapters.python = {
+        type = "executable",
+        command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python",
+        args = { "-m", "debugpy.adapter" },
+      }
+
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return "/usr/bin/python3"
+          end,
+        },
+      }
+
+      -- キーマップ
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step Out" })
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+    end,
   },
   {
     "rcarriga/nvim-dap-ui",
@@ -10,7 +67,8 @@ return {
       local dapui = require("dapui")
       dapui.setup()
 
-      -- デバッグ開始/終了時にUIを自動開閉
+      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "DAP UI: Toggle" })
+
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
       end
@@ -20,6 +78,16 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = { "mason-org/mason.nvim", "mfussenegger/nvim-dap" },
+    config = function()
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "codelldb", "python" },
+        automatic_installation = true,
+      })
     end,
   },
 }
