2cbae315421f7cc96e9e44228a5c118ff9180099
Shisato Yano <yano@8kt.jp>
2026-07-16 19:18:05 +0900

    feat: 🎸 add key binds for wezterm

--- wezterm/config/keybinds.lua
+++ wezterm/config/keybinds.lua
@@ -12,6 +12,16 @@ function M.setup(config)
     { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
     { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
     { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+    -- コピーモードに入る(デフォルトのCtrl+Shift+Xは押しにくいためleader経由に変更)
+    { key = "c", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+    -- スクロールバックを1ページ分移動(PageUp/PageDownはFn併用が必要で押しにくいため)
+    { key = "u", mods = "LEADER", action = wezterm.action.ScrollByPage(-1) },
+    { key = "d", mods = "LEADER", action = wezterm.action.ScrollByPage(1) },
+    -- ペインサイズの微調整(LEADER + 矢印キーで2セル分ずつリサイズ)
+    { key = "LeftArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Left", 2 }) },
+    { key = "RightArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Right", 2 }) },
+    { key = "UpArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Up", 2 }) },
+    { key = "DownArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Down", 2 }) },
     -- ライト/ダーク配色を瞬時に切り替え
     { key = "t", mods = "LEADER", action = wezterm.action.EmitEvent("toggle-color-scheme") },
     -- WezTermを終了(全ウィンドウ/タブを閉じる)
