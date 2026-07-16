b268d1132023eadaba5da23d7f154f5e22509bd6
Shisato Yano <yano@8kt.jp>
2026-07-16 19:16:48 +0900

    feat: 🎸 add symbol search commands

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,18 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in project" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List open buffers" })
     vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "List diagnostics (errors/warnings)" })
+    vim.keymap.set(
+      "n",
+      "<leader>fs",
+      builtin.lsp_document_symbols,
+      { desc = "List classes/functions/variables in current file" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>fS",
+      builtin.lsp_dynamic_workspace_symbols,
+      { desc = "List classes/functions/variables in whole project" }
+    )
     vim.keymap.set("n", "<leader>?", function()
       require("telescope.builtin").keymaps()
     end, { desc = "Show all keymaps" })
