4a30269c1b2a027c0d19d814262dcc17f21dabec
Shisato Yano <yano@8kt.jp>
2026-07-26 20:44:38 +0900

    feat: 🎸 add alias to open notification relay log on terminal

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -76,6 +76,9 @@ dstop() {
   docker ps --format '{{.Names}}\t{{.Image}}\t{{.Status}}' | fzf --reverse --multi --header="NAMES	IMAGE	STATUS" | cut -f1 | xargs -r docker stop
 }
 
+# notify-relayが検知した通知をリアルタイムに表示する
+alias nrlog="tail -f ~/.local/state/notify-relay/notify.log"
+
 # 自分に関するPRを横断で確認する(自分が出したもの/レビュー依頼が来ているもの)
 prs() {
   echo "=== 自分が出したPR ==="
