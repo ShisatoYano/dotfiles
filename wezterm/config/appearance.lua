094a4e3d36dce86c1d5ab30d4cb4bb62938a6734
Shisato Yano <yano@8kt.jp>
2026-08-02 21:57:44 +0900

    style: 💄 dim inactive panes to highlight active pane

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -32,6 +32,11 @@ function M.setup(config)
   -- "Hold"だと、wezterm-sessionsが復元処理で不要になった初期タブを閉じるために
   -- 送る"exit"コマンドの後もペインが残ってしまうため、正常終了時は自動で閉じる設定にする
   config.exit_behavior = "CloseOnCleanExit"
+  -- 非アクティブなペインを暗く/彩度を落として表示し、アクティブなペインが一目で分かるようにする
+  config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.2,
+  }
 end
 
 return M
