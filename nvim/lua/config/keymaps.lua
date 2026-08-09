3cdfffcfc4454c8905bee6ebd194b16c3b3e50d4
Shisato Yano <yano@8kt.jp>
2026-08-09 14:52:34 +0900

    feat: 🎸 add Vimium C cheatsheet keybinding

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "<leader>gh", "<cmd>edit ~/dotfiles/docs/git-cheatsheet.md<C
 vim.keymap.set("n", "<leader>nh", "<cmd>edit ~/dotfiles/docs/nvim-cheatsheet.md<CR>", { desc = "Open nvim cheatsheet" })
 vim.keymap.set("n", "<leader>wh", "<cmd>edit ~/dotfiles/docs/terminal-cheatsheet.md<CR>", { desc = "Open terminal cheatsheet" })
 vim.keymap.set("n", "<leader>dh", "<cmd>edit ~/dotfiles/docs/docker-cheatsheet.md<CR>", { desc = "Open docker cheatsheet" })
+vim.keymap.set("n", "<leader>vh", "<cmd>edit ~/dotfiles/docs/vimium-cheatsheet.md<CR>", { desc = "Open vimium cheatsheet" })
 
 -- LSPがバッファにアタッチされたときに呼ばれるキーマップ設定
 function M.on_lsp_attach(args)
