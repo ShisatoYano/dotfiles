0f5f3701427b7fb50977cff7222bb7c0a30ef015
Shisato Yano <yano@8kt.jp>
2026-08-15 18:03:45 +0900

    refactor: 💡 clarify keybinds load order and wire up worktree_workspace module

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,10 @@ local config = wezterm.config_builder()
 
 require("config.theme") -- toggle-color-schemeイベントを登録するため読み込むだけでOK
 require("config.appearance").setup(config)
-require("config.keybinds").setup(config)
+require("config.keybinds").setup(config) -- config.keys/config.key_tablesを直接代入するので最初に呼ぶ
 require("config.cmdpicker").setup(config)
 require("config.tab_title").setup(config)
 require("config.sessions").setup(config)
+require("config.worktree_workspace").setup(config)
 
 return config
