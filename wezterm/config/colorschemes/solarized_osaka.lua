d5d5fb159285d367274481fdc24b4593a5f80425
Shisato Yano <yano@8kt.jp>
2026-08-20 23:07:45 +0900

    feat: 🎸 Add solarized-osaka colorscheme for WezTerm

---
+++ wezterm/config/colorschemes/solarized_osaka.lua
@@ -0,0 +1,24 @@
+-- craftzdog/solarized-osaka.nvim (dark)の配色を、同梱のWezTermジェネレーターで書き出したものをそのまま移植
+-- 生成元: solarized-osaka.nvim の lua/solarized-osaka/extra/wezterm.lua
+return {
+  foreground = "#839395",
+  background = "#001419",
+  cursor_bg = "#839395",
+  cursor_border = "#839395",
+  cursor_fg = "#001419",
+  selection_bg = "#1a6397",
+  selection_fg = "#839395",
+
+  ansi = { "#001419", "#db302d", "#849900", "#b28500", "#268bd3", "#d23681", "#29a298", "#9eabac" },
+  brights = { "#001419", "#db302d", "#849900", "#b28500", "#268bd3", "#d23681", "#29a298", "#839395" },
+
+  tab_bar = {
+    inactive_tab_edge = "#002c38",
+    background = "#191b28",
+    active_tab = { fg_color = "#268bd3", bg_color = "#001419" },
+    inactive_tab = { bg_color = "#002c38", fg_color = "#063540" },
+    inactive_tab_hover = { bg_color = "#002c38", fg_color = "#268bd3" },
+    new_tab_hover = { fg_color = "#002c38", bg_color = "#268bd3" },
+    new_tab = { fg_color = "#268bd3", bg_color = "#191b28" },
+  },
+}
