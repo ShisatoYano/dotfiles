b21591e1afbe6da44fa798b01bce677d75ad19da
Shisato Yano <yano@8kt.jp>
2026-09-20 18:00:57 +0900

    feat: 🎸 Enable treesitter highlighting for markdown and json

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,12 +5,14 @@ return {
   config = function()
     local ts = require("nvim-treesitter")
     ts.setup()
-    ts.install({ "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "xml" })
+    -- markdown_inlineはfiletypeではないが、markdownが本文中のリンク・強調等を
+    -- インジェクションで解釈するのに必要なため、インストール対象にだけ含める
+    ts.install({ "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "xml", "markdown", "markdown_inline", "json" })
 
     vim.api.nvim_create_autocmd("FileType", {
       -- .shのfiletypeはbashではなくshになるため、shも対象にしないと
       -- 従来のsh syntaxにフォールバックし、here-string(<<<)以降がヒアドキュメント扱いで崩れる
-      pattern = { "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "sh", "xml" },
+      pattern = { "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "sh", "xml", "markdown", "json" },
       callback = function()
         vim.treesitter.start()
       end,
