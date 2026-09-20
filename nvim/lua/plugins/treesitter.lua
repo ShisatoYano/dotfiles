6b9ef10298ed5579f1f6f0bffa78aa9567f35234
Shisato Yano <yano@8kt.jp>
2026-09-20 17:40:07 +0900

    fix: 🐛 Enable treesitter highlighting for sh filetype

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,9 @@ return {
     ts.install({ "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "xml" })
 
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "xml" },
+      -- .shのfiletypeはbashではなくshになるため、shも対象にしないと
+      -- 従来のsh syntaxにフォールバックし、here-string(<<<)以降がヒアドキュメント扱いで崩れる
+      pattern = { "cpp", "c", "python", "cmake", "yaml", "lua", "bash", "sh", "xml" },
       callback = function()
         vim.treesitter.start()
       end,
